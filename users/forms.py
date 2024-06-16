e427c1b4ef8e4b066e200a7f28eae56d98984f17
Kuzkin Ilya <heartsong_@mail.ru>
2024-06-16 20:59:18 +0500

    добавить файл users/forms.py

---
+++ users/forms.py
@@ -0,0 +1,38 @@
+import uuid
+from datetime import timedelta
+
+from django import forms
+from django.utils.timezone import now
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm, UserChangeForm
+
+from common.views import StyleFormMixin
+from users.models import User
+
+
+class UserLoginForm(StyleFormMixin, AuthenticationForm):
+    """
+    Класс для работы с формой "UserLoginForm"
+    """
+    class Meta:
+        model = User
+        fields = ('email', 'password')
+
+
+class UserRegistrationForm(StyleFormMixin, UserCreationForm):
+    """
+    Класс для работы с формой "UserRegistrationForm"
+    """
+
+    class Meta:
+        model = User
+        fields = ('first_name', 'last_name', 'email', 'password1', 'password2')
+
+
+class UserProfileForm(StyleFormMixin, UserChangeForm):
+    """
+    Класс для работы с формой "UserProfileForm"
+    """
+
+    class Meta:
+        model = User
+        fields = ('first_name', 'last_name', 'image', 'email', 'phone', 'country')
