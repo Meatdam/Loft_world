747c41f60e4754771f36e74ff44808e8ab1b7012
Kuzkin Ilya <heartsong_@mail.ru>
2024-06-16 21:09:03 +0500

    добавить файл users/urls.py

---
+++ users/urls.py
@@ -0,0 +1,15 @@
+from django.urls import path
+from django.contrib.auth.decorators import login_required
+from users.views import UserLoginView, UserRegistrationCreateView, ProfileUpdateView, logaut, ConfirmRegister, \
+    reset_password
+
+app_name = 'users'
+
+urlpatterns = [
+    path('login/', UserLoginView.as_view(), name='login'),
+    path('registration/', UserRegistrationCreateView.as_view(), name='registration'),
+    path('profile/<int:pk>/', login_required(ProfileUpdateView.as_view()), name='profile'),
+    path('logout/', logaut, name='logout'),
+    path('confirm/', ConfirmRegister.as_view(), name='confirm'),
+    path('reset_password/', reset_password, name='reset_password'),
+]
