06ff703e849c6d9f9daca3dd5e4ec6b8dd9e0e82
Kuzkin Ilya <heartsong_@mail.ru>
2024-06-21 15:45:26 +0500

    Добавить измененный файл services/admin.py

--- services/admin.py
+++ services/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
-# Register your models here.
+from services.models import ServicCategory, LoftService
+
+
+@admin.register(ServicCategory)
+class ServicCategoryAdmin(admin.ModelAdmin):
+    list_display = ('title', 'description', 'image')
+    list_filter = ('title',)
+
+
+@admin.register(LoftService)
+class LoftServiceAdmin(admin.ModelAdmin):
+    list_display = ('title', 'description', 'image', 'category', 'price', 'views_count', 'created_at')
+    list_filter = ('title',)
