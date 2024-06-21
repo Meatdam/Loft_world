9bad95c5d0714326aefbb74e2c8b1212432c2fad
Kuzkin Ilya <heartsong_@mail.ru>
2024-06-21 15:29:23 +0500

    Добавить измененный файл models.py
    добавить форму отзывов "Blog"

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+from users.models import User
+
+NULLABLE = {'blank': True, 'null': True}
+
+
+class Blog(models.Model):
+    title = models.CharField(max_length=250, verbose_name='Заголовок')
+    description = models.TextField(verbose_name='описание')
+    image = models.ImageField(upload_to='blog/', verbose_name='картинка', **NULLABLE)
+    owner = models.ForeignKey(to=User, on_delete=models.CASCADE, verbose_name='Автор', **NULLABLE)
+
+    def __str__(self):
+        return f"{self.title}"
+
+    class Meta:
+        verbose_name = 'Блог'
+        verbose_name_plural = 'Блоги'
