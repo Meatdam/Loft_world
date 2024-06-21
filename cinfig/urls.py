c44b61dc2845aca8f7c892d265ba87d7bf1c8c88
Kuzkin Ilya <heartsong_@mail.ru>
2024-06-21 13:48:30 +0500

    Добавить файл urls.py
    Добавить приложение "blog" в urlpatterns проектa

--- cinfig/urls.py
+++ cinfig/urls.py
@@ -25,4 +25,5 @@ urlpatterns = [
     path('users/', include('users.urls', namespace='users')),
     path('srvices/', include('services.urls', namespace='srvices')),
     path('mailing/', include('mailing.urls', namespace='mailing')),
+    path('blog/', include('blog.urls', namespace='blog')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
